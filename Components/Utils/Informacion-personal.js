0e553387b3dc6e7eead1c946ad135ff379296d48
Asanchez90 <alesp1990@gmail.com>
2023-08-04 20:41:05 +0200

    cambios en informacion persona, ortografia y light

--- Components/Utils/Informacion-personal.js
+++ Components/Utils/Informacion-personal.js
@@ -4,8 +4,8 @@ export const infoPersonal = [
     Foto: 'https://res.cloudinary.com/dcs7ntcz7/image/upload/v1691149366/portfolio/aplastandome_kzpkws.jpg',
     alt: 'imagen-Foto',
     pregunta:'Un poco sobre mi...',
-    respuesta: ` Soy Alejandro, he tenido que pegar un giro de 180º en mi sector laboral por completo, de comisario de averías
-     a desarrollador web. por razones obvia... empezaba a sentir como me aplastaba el trabajo....`,
+    respuesta: ` Soy Alejandro, he tenido que dar un giro de 180º en mi sector laboral por completo, de comisario de averías
+     a desarrollador web. por razones obvia, empezaba a sentir como me aplastaba el trabajo....`,
   },
 
   {
@@ -13,10 +13,10 @@ export const infoPersonal = [
     Foto: 'https://res.cloudinary.com/dcs7ntcz7/image/upload/v1691149366/portfolio/marine-surveyor_t0lvpg.jpg',
     alt: 'imagen-Foto',
     pregunta:'¿Cual es tu formación?',
-    respuesta: `En cuanto a mi formacion, soy Tecnico administrativo y financiero, 
+    respuesta: `En cuanto a mi formación, soy Técnico administrativo y financiero, 
         Comisario de averías titulado por el Colegio de Oficiales de la Marina Mercante. 
-        La combinacion de estos estudios me han permitido trabajar y colaborar con distintos 
-        gabinetes de diferentes aseguradoras a nivel nacional e internacional, asi como trabajar
+        La combinación de estos estudios me han permitido trabajar y colaborar con distintos 
+        gabinetes de diferentes aseguradoras a nivel nacional e internacional, así como trabajar
         con las principales navieras lideres en el sector naval.  
         `,
   },
@@ -26,13 +26,14 @@ export const infoPersonal = [
     Foto: 'https://res.cloudinary.com/dcs7ntcz7/image/upload/v1691149367/portfolio/foto2_ujmvj5.jpg',
     alt: 'imagen-Foto',
     pregunta:'¿Que te ha llevado a hacer un portfolio?',
-    respuesta: `A finales del 2022 decicí emprender un cambio en el sector profesional y aprender 
-        una nueva profesion de desarrollador FullStack Junior en The Power Business School. Mi principal 
-        motivacion para hacer este gran cambio ha sido el desarrollar una app y una spwa para la empresa 
-        en la que estaba trabajando, además de solventar problemas/tiempo de comunicación con el cliente final.
-        Estoy emocionado por lo que el futuro me depara en este campo, y estoy comprometido a seguir creciendo e
-        investigando sobre nuevas tecnologías para mejorar en mi carrera como desarrollador 
-        FullStack Junior.
+    respuesta: `A finales del 2022 decidí emprender un cambio en el sector profesional y aprender 
+    una nueva profesión de desarrollador FullStack Junior en The Power Business School. Mi principal 
+    motivación para hacer este gran cambio ha sido el desarrollar una app y una spwa para la empresa 
+    en la que estaba trabajando, además de solventar problemas, gestion de los tiempos de comunicación con el cliente final.
+    Estoy emocionado por lo que el futuro me depara en este campo, y estoy comprometido a seguir creciendo e
+    investigando sobre nuevas tecnologías para mejorar en mi carrera como desarrollador 
+    FullStack Junior.
+
 
         `,
   },
@@ -43,7 +44,7 @@ export const infoPersonal = [
     alt: 'imagen-Foto',
     pregunta:'¿Como eres?',
     respuesta: ` En cuanto a mi personalidad me considero una persona optimista, creativa, resolutiva, adaptativa al entorno y
-    condiciones de trabajo, capacidad de trabajo en equipo, involucrado con los proyectos...
+    condiciones de trabajo, capacidad de trabajo en equipo, involucrado con los proyectos,etc.
 
 
         `,
